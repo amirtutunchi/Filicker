1d218b098bd6d5c3e41efe38450855f7fd0e04ac
amir tutunchi <amirbt@live.com>
2021-09-20 05:01:09 +0430

    Initial Commit

---
+++ Filicker/FilickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilickerApp.swift
+//  Filicker
+//
+//  Created by Amir Tutunchi on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct FilickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
